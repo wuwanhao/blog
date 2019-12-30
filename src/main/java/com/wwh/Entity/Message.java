a3836cfb419398f27db19cd3cecd7dc08189e850
武万浩 <1461282363@qq.com>
2019-12-30 20:52:20 +0800

    留言0

---
+++ src/main/java/com/wwh/Entity/Message.java
@@ -0,0 +1,45 @@
+package com.wwh.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //创建时间
+    private Date createDate;
+
+    //留言内容
+    private String content;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
